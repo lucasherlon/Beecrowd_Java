a72b6d5680428f65cd83128473ac9d982aca0667
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-09-24 19:34:09 -0300

    Problema: 1300 - Horas e Minutos

--- HorasMinutos.java
+++ HorasMinutos.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class HorasMinutos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner leitor = new Scanner(System.in);
+		int x;
+		
+		while(leitor.hasNextInt()) {
+			x = leitor.nextInt();
+			
+			if(x%6==0) {
+				System.out.println("Y");
+			}else {
+				System.out.println("N");
+			}
+		}
 	}
 
 }
