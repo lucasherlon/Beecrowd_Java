977e126ae4a7ded282cf3287cd05f74edbf4e864
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-09-29 19:34:07 -0300

    Problema: 3307 - Decora o Rolê!

--- DecoraORole.java
+++ DecoraORole.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 
 public class DecoraORole {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner leitor = new Scanner(System.in);
+		double area, raio, preco;
+		
+		int n = leitor.nextInt();
+		
+		for(int i=0;i<n;i++) {
+			area = leitor.nextDouble();
+			raio = Math.sqrt(area/12.56);
+			
+			if(raio < 12) {
+				preco = area*0.09;
+				System.out.printf("vermelho = R$ %.2f\n", preco);
+			}else if(raio >= 12 && raio <= 15) {
+				preco = area*0.07;
+				System.out.printf("azul = R$ %.2f\n", preco);
+			}else {
+				preco = area*0.05;
+				System.out.printf("amarelo = R$ %.2f\n", preco);
+			}
+			
+			
+		}
 	}
 
 }
